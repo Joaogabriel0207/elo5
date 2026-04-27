519acc48d6a30d9b10d6de3e0f6368c04d623c60
Joaogabriel0207 <149544641+Joaogabriel0207@users.noreply.github.com>
2026-04-27 11:22:39 -0300

    teste5

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-226.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-dff7d825' );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-ead41114', {"layout":"number","heading":"Membros da Equipe","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":25,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-eb69c34f', {"layout":"number","heading":"Anos de Experi\u00eancia","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":12,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
